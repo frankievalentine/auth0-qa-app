4c9492c41e696cffdd6fde59462a136f9956d70b
frankievalentine <frankievalentine9@gmail.com>
2018-12-28 18:12:30 -0800

    Added specific question routing functionality, prettier edits, etc.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import {Route} from 'react-router-dom';
 import NavBar from './NavBar';
+import Questions from './Questions/Questions';
+import Question from './Question/Question';
 
 class App extends Component {
   render() {
     return (
       <div>
         <NavBar />
-        <p>Work in progress.</p>
+        <Route exact path='/' component={Questions} />
+        <Route exact path='/question/:questionId' component={Question} />
       </div>
     );
   }
