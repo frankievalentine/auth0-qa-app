15540a67822311bd4c06ed67d8ae483410f41f96
frankievalentine <frankievalentine9@gmail.com>
2018-12-28 17:13:48 -0800

    Added front-end to application and react-router

---
+++ backend/src/index.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+// initialize express
+const app = express();
+
+// empty array acts as db, in place of a MySQL or PostreSQL or MongoDB
+const questions = [];
+
+// enhance app security using Helmet
+app.use(helmet());
+
+// body-parser to parse application/json content-type
+app.use(bodyParser.json());
+
+// enable all CORS requests
+app.use(cors());
+
+// log HTTP requests
+app.use(morgan("combined"));
+
+// retrieve all questions
+app.get("/", (req, res) => {
+  const qs = questions.map(q => ({
+    id: q.id,
+    title: q.title,
+    description: q.description,
+    answers: q.answers.length,
+  }));
+  res.send(qs);
+});
+
+// get a specific question
+app.get("/:id", (req, res) => {
+  const question = questions.filter(q => q.id === parseInt(req.params.id));
+  if (question.length > 1) return res.status(500).send();
+  if (question.length === 0) return res.status(404).send();
+  res.send(question[0]);
+});
+
+// insert a new question
+app.post("/", (req, res) => {
+  const { title, description } = req.body;
+  const newQuestion = {
+    id: questions.length + 1,
+    title,
+    description,
+    answers: [],
+  };
+  questions.push(newQuestion);
+  res.status(200).send();
+});
+
+// insert a new answer to a question
+app.post("/answer/:id", (req, res) => {
+  const { answer } = req.body;
+
+  const question = questions.filter(q => q.id === parseInt(req.params.id));
+  if (question.length > 1) return res.status(500).send();
+  if (question.length === 0) return res.status(404).send();
+  
+  question[0].answers.push({
+      answer,
+  });
+
+  res.status(200).send();
+});
+
+// start server
+app.listen(8081, () => {
+    console.log('listening on port 8081');
+});
